605eb2569986cffff99df3fac26632771387f400
zdmgg <zachdwaynemglindro@gmail.com>
2023-12-17 17:22:46 +0800

    Initial commit

---
+++ regression/regression.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+
+# Generate augmented coefficient matrix for polynomial regression
+def generate_augcoeffmatrix(data, order):
+    augcoeffmatrix = pd.DataFrame(np.zeros((order + 1, order + 2)))
+
+    x = data.iloc[:, 0]
+    y = data.iloc[:, 1]
+
+    # Fill LHS of augcoeffmatrix
+    for i in range(order + 1):
+        for j in range(order + 1):
+            augcoeffmatrix.iloc[i, j] = sum(x**(i+j))
+
+    # Fill RHS of augcoeffmatrix
+    for i in range(order + 1):
+        augcoeffmatrix.iloc[i, -1] = sum(y * x**i)
+
+    return augcoeffmatrix
+
+# Performs Gauss-Jordan elimination on a dataframe
+def gauss_jordan(df):
+    for i in range(len(df.index)):
+        # Divide row by pivot
+        df.iloc[i] /= df.iloc[i, i]
+
+        # Subtract row from other rows
+        for j in range(len(df.index)):
+            if i != j:
+                df.iloc[j] -= df.iloc[i] * df.iloc[j, i]
+
+    return df
+
+# Creates function for polynomial regression based on augcoeffmatrix
+def create_function(df):
+    function_parts = [f"{df.iloc[i, -1]}*(x**{i})" for i in range(len(df.index))]
+    function = 'lambda x: ' + ' + '.join(function_parts)
+    return function
+
+# Get data points
+file = 'data.csv'
+data = pd.read_csv(file, sep='|')
+
+order = 2 # Set order of polynomial for regression
+
+augcoeffmatrix = generate_augcoeffmatrix(data, order)
+augcoeffmatrix = gauss_jordan(augcoeffmatrix)
+
+f = eval(create_function(augcoeffmatrix))
+print(f(50))
---
+++ simplex/solver.py
@@ -0,0 +1,121 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+# Takes in normal maximization matrix
+def maximize(df, max_iterations = 5000):
+    df = df.astype(float)
+    i = 0
+
+    while df.iloc[-1].lt(0).any() and i < max_iterations:
+        # Select pivot column
+        pivot_column = df.iloc[-1].idxmin()
+
+        # Select pivot row
+        ratios = df.iloc[:, -1] / df.loc[:, pivot_column]
+        ratios = ratios[:-1]
+        print(ratios)
+        pivot_row = ratios[ratios.gt(0)].idxmin()
+
+        # Normalize pivot row
+        df.loc[pivot_row] /= df.loc[pivot_row, pivot_column]
+
+        # Clear pivot column
+        for row in df.index:
+            if row != pivot_row:
+                df.loc[row] -= df.loc[row, pivot_column] * df.loc[pivot_row]
+
+        i += 1
+    
+    print(f'Iterations: {i}')
+    return df
+
+# Takes in matrix post-transposition
+# 1:n-1 columns are constraints
+# n column is the original objective function
+# form: ax=b
+def minimize(df, max_iterations = 5000):
+    df = df.reset_index(drop=True)
+
+    # Add variable names
+    df.columns = [f's{i}' for i in range(1, len(df.columns))] + ['rhs']
+
+    # Add slack variables
+    for i in range(1, len(df.index)):
+        df.insert(len(df.columns)-1, f'x{i}', 0)
+    df.insert(len(df.columns)-1, 'z', 0)
+
+    # Make last row negative
+    df.iloc[-1] = df.iloc[-1] * -1
+
+    # Add 1s diagonally to the slack variables, move the z coefficient to proper place
+    for i in range(len(df.index)):
+        df.iloc[i, len(df.columns)-len(df.index)-1+i] = 1
+    df.iloc[-1, -1] = 0
+
+    return maximize(df, max_iterations)
+
+# Set up data
+constraints = pd.read_csv('constraints.tsv', sep='\t', index_col=0)
+
+raw_data = pd.read_csv('food_data.tsv', sep='\t', index_col=0)
+raw_data['Price/Serving'] = raw_data['Price/Serving'].str.replace('$', '').astype(float)
+
+test_data = raw_data.loc[['Frozen Broccoli',
+                          'Carrots,Raw',
+                          'Celery, Raw',
+                          'Frozen Corn',
+
+                          'Lettuce,Iceberg,Raw',
+                          'Roasted Chicken',
+                          'Potatoes, Baked',
+                          'Tofu',
+
+                          'Peppers, Sweet, Raw',
+                          'Spaghetti W/ Sauce',
+                          'Tomato,Red,Ripe,Raw',
+
+                          'Apple,Raw,W/Skin',
+                          'Banana',
+                          'Grapes',
+
+                          'Kiwifruit,Raw,Fresh',
+                          'Oranges',
+                          'Bagels',
+                          'Wheat Bread',
+
+                          'White Bread',
+                          'Oatmeal Cookies'], :]
+
+# Prep test data
+test_data = test_data.drop(columns=['Serving Size'])
+
+# Swap Price/Serving and last column because Price is what we're minimizing
+col_list = list(test_data.columns)
+price_index = col_list.index('Price/Serving')
+last_index = len(col_list) - 1
+col_list[price_index], col_list[last_index] = col_list[last_index], col_list[price_index]
+test_data = test_data[col_list]
+
+# Add the minimum and maximum constraints for the nutrients in the last row
+negatives = test_data.copy().multiply(-1).iloc[:, :-1].add_suffix('_max')
+test_data = pd.concat([negatives, test_data], axis=1)
+
+min = pd.DataFrame(constraints['Min']).transpose().reset_index(drop=True)
+max = pd.DataFrame(constraints['Max']).transpose().multiply(-1).reset_index(drop=True).add_suffix('_max')
+cons = pd.concat([max,min], axis=1)
+
+test_data = pd.concat([test_data, cons], axis=0)
+test_data.iloc[-1, -1] = 0
+
+# Each food has max 10 serving
+# Insert new diagonal matrix before last column
+for i in range(len(test_data.index)-1):
+    test_data.insert(len(test_data.columns)-1, f'max_{i}', 0)
+
+for i in range(len(test_data.index)-1):
+    test_data.iloc[i, len(test_data.columns)-len(test_data.index)-1+i+1] = -1
+    test_data.iloc[len(test_data.index)-1, len(test_data.columns)-len(test_data.index)-1+i+1] = -10
+
+
+solved = minimize(test_data)
+print(solved)
