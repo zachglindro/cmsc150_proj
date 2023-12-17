afcd2f66a681666033b0fe5a3d79be5feaba99c6
zdmgg <zachdwaynemglindro@gmail.com>
2023-12-17 23:08:02 +0800

    Refactor solver

--- simplex/solver.py
+++ simplex/solver.py
@@ -1,6 +1,6 @@
 import pandas as pd
-
 pd.set_option('display.max_columns', None)
+
 # Takes in normal maximization matrix
 def maximize(df, max_iterations = 5000):
     df = df.astype(float)
@@ -13,7 +13,10 @@ def maximize(df, max_iterations = 5000):
         # Select pivot row
         ratios = df.iloc[:, -1] / df.loc[:, pivot_column]
         ratios = ratios[:-1]
-        print(ratios)
+
+        if ratios.le(0).all():
+            return 404
+
         pivot_row = ratios[ratios.gt(0)].idxmin()
 
         # Normalize pivot row
@@ -26,12 +29,11 @@ def maximize(df, max_iterations = 5000):
 
         i += 1
     
-    print(f'Iterations: {i}')
     return df
 
 # Takes in matrix post-transposition
 # 1:n-1 columns are constraints
-# n column is the original objective function
+# nth column is the original objective function
 # form: ax=b
 def minimize(df, max_iterations = 5000):
     df = df.reset_index(drop=True)
@@ -54,47 +56,41 @@ def minimize(df, max_iterations = 5000):
 
     return maximize(df, max_iterations)
 
-# Set up data
+# Read constraints.tsv
 constraints = pd.read_csv('constraints.tsv', sep='\t', index_col=0)
 
+# Read food_data.tsv
 raw_data = pd.read_csv('food_data.tsv', sep='\t', index_col=0)
+
+# Remove $ from Price/Serving
 raw_data['Price/Serving'] = raw_data['Price/Serving'].str.replace('$', '').astype(float)
 
 test_data = raw_data.loc[['Frozen Broccoli',
                           'Carrots,Raw',
                           'Celery, Raw',
                           'Frozen Corn',
-
                           'Lettuce,Iceberg,Raw',
                           'Roasted Chicken',
                           'Potatoes, Baked',
                           'Tofu',
-
                           'Peppers, Sweet, Raw',
                           'Spaghetti W/ Sauce',
                           'Tomato,Red,Ripe,Raw',
-
                           'Apple,Raw,W/Skin',
                           'Banana',
                           'Grapes',
-
                           'Kiwifruit,Raw,Fresh',
                           'Oranges',
                           'Bagels',
                           'Wheat Bread',
-
                           'White Bread',
                           'Oatmeal Cookies'], :]
 
-# Prep test data
+# Remove Serving Size column because it's not needed for simplex
 test_data = test_data.drop(columns=['Serving Size'])
 
 # Swap Price/Serving and last column because Price is what we're minimizing
-col_list = list(test_data.columns)
-price_index = col_list.index('Price/Serving')
-last_index = len(col_list) - 1
-col_list[price_index], col_list[last_index] = col_list[last_index], col_list[price_index]
-test_data = test_data[col_list]
+test_data = test_data[[c for c in test_data.columns if c != 'Price/Serving'] + ['Price/Serving']]
 
 # Add the minimum and maximum constraints for the nutrients in the last row
 negatives = test_data.copy().multiply(-1).iloc[:, :-1].add_suffix('_max')
@@ -107,13 +103,20 @@ cons = pd.concat([max,min], axis=1)
 test_data = pd.concat([test_data, cons], axis=0)
 test_data.iloc[-1, -1] = 0
 
-# Each food has max 10 serving
-# Insert new diagonal matrix before last column
-for i in range(len(test_data.index)-1):
-    test_data.insert(len(test_data.columns)-1, f'max_{i}', 0)
+# Each food has max 10 serving. Add this constraint
+num_rows, num_cols = test_data.shape
 
-for i in range(len(test_data.index)-1):
+# Insert rows for max serving constraint
+for i in range(num_rows - 1):
+    before_last_column = len(test_data.columns) - 1
+    test_data.insert(before_last_column, f'max_{i}', 0)
+
+
+for i in range(num_rows - 1):
+    # Set -1s diagonally for max serving constraint
     test_data.iloc[i, len(test_data.columns)-len(test_data.index)-1+i+1] = -1
+
+    # Set bottom row to -10 for max serving constraint
     test_data.iloc[len(test_data.index)-1, len(test_data.columns)-len(test_data.index)-1+i+1] = -10
 
 
